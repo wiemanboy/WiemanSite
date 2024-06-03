04b426ee24459cb99948efc3a51c582006e5662e
jarno <wiemanboy@gmail.com>
2024-06-03 20:42:15 +0200

    ADD: Placeholder.svelte component as a placeholder

--- src/routes/[[locale=supportedLocale]]/+page.svelte
+++ src/routes/[[locale=supportedLocale]]/+page.svelte
@@ -3,12 +3,11 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	import { t } from "svelte-i18n";
-	import MdiGithub from "virtual:icons/mdi/github";
 	import type QuotesRepository from "$lib/data/quotes/QuotesRepository";
 	import type QuoteDto from "$lib/dtos/quotes/QuoteDto";
 	import types from "$lib/types";
 	import { container } from "../../../inversify.config";
-	import Link from "../../components/Link.svelte";
+	import UnderConstruction from "../../components/Placeholder.svelte";
 
 	const quotesRepository = container.get<QuotesRepository>(types.quotesRepository);
 
@@ -20,10 +19,9 @@
 
 </script>
 
-<main class="flex h-screen flex-grow flex-col items-center justify-center text-center">
-	<h1 class="flex text-4xl sm:text-7xl">{$t("underConstruction")}!</h1>
-	<div class="mx-2 flex flex-grow flex-col justify-center">
-		<div class="front-themed rounded border p-4">
+<main class="flex flex-grow flex-col h-screen">
+	<UnderConstruction class="flex flex-grow flex-col h-max m-5">
+		<div class="front-themed rounded border p-4 mx-10 flex flex-col text-center">
 			<div class="mb-2 font-bold">{$t("quotes.message")}:</div>
 			{#await promise}
 				<div>{$t("quotes.loading")}...</div>
@@ -37,12 +35,5 @@
 				<div class="underline">{error.message}</div>
 			{/await}
 		</div>
-		<Link
-			class="flex justify-center"
-			to="https://github.com/wiemanboy"
-			undecorated
-		>
-			<MdiGithub />
-		</Link>
-	</div>
+	</UnderConstruction>
 </main>
