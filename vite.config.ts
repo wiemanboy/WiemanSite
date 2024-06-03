bbc3ec07f27aeff9c24571ce50dc9cf147c855a0
jarno <wiemanboy@gmail.com>
2024-06-03 17:20:18 +0200

    ADD: additional test support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from "vitest/config";
+import { configDefaults, coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [sveltekit(), Icons({ compiler: "svelte", autoInstall: true })],
 	test: {
 		include: ["tests/**/*.{test,spec}.{js,ts}"],
-		exclude: ["tests/**/*.integration.{test,spec}.{js,ts}"],
+		exclude: [...configDefaults.exclude, "tests/**/*.integration.{test,spec}.{js,ts}"],
 		coverage: {
 			reportsDirectory: "test-results/coverage",
+			exclude: [...coverageConfigDefaults.exclude, "**/*.config.{js,ts}", "**/i18n.ts"],
 		},
 	},
 	server: {
