969aa4e066fdcc49457b44d98caa7c8e7b002443
jarno <wiemanboy@gmail.com>
2024-06-25 20:11:17 +0200

    ADD: ProfileBanner.svelte

---
+++ src/components/profile/ProfileBanner.svelte
@@ -0,0 +1,20 @@
+<!--
+Banner
+
+-->
+
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements";
+
+	interface $$Props extends HTMLAttributes<HTMLDivElement> {
+		name: string;
+	}
+
+	export let name: string;
+</script>
+
+<div {...$$restProps}>
+	<div class="front-themed rounded border p-2 font-extrabold">
+		<div>{name}</div>
+	</div>
+</div>
--- src/routes/[[locale=supportedLocale]]/+page.svelte
+++ src/routes/[[locale=supportedLocale]]/+page.svelte
@@ -1,42 +1,15 @@
 <!-- Home page -->
 
 <script lang="ts">
-	import { onMount } from "svelte";
 	import { t } from "svelte-i18n";
-	import type QuotesRepository from "$lib/data/quotes/QuotesRepository";
-	import type QuoteDto from "$lib/dtos/quotes/QuoteDto";
-	import types from "$lib/types";
-	import { container } from "../../../inversify.config";
-	import UnderConstruction from "../../components/Placeholder.svelte";
+	import ProfileBanner from "../../components/profile/ProfileBanner.svelte";
 
-	const quotesRepository = container.get<QuotesRepository>(types.quotesRepository);
 	const tBase = "home.";
-
-	let promise: Promise<QuoteDto>;
-
-	onMount(() => {
-		promise = quotesRepository.getRandomQuote();
-	});
 
 </script>
 
 <main class="flex flex-grow flex-col h-screen">
-	<UnderConstruction class="flex flex-grow flex-col h-max m-5">
-		<div class="front-themed rounded border p-4 mx-10 flex flex-col text-center">
-			<div class="mb-2 font-bold">{$t("quotes.message")}:</div>
-			{#await promise}
-				<div>{$t("quotes.loading")}...</div>
-			{:then quote}
-				{#if quote !== undefined}
-					<div class="lowercase">{quote.quote}</div>
-					<div>- {quote.author}</div>
-				{/if}
-			{:catch error}
-				<div>{$t("quotes.error")}!</div>
-				<div class="underline">{error.message}</div>
-			{/await}
-		</div>
-	</UnderConstruction>
+	<ProfileBanner class="m-3" name="Jarno Wieman" />
 </main>
 
 <svelte:head>
