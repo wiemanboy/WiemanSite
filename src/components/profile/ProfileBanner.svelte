ba332693cf68a90b98bff5249ad2c6bc290ce4eb
jarno <wiemanboy@gmail.com>
2024-07-03 16:01:46 +0200

    ADD: profile image with unpic

--- src/components/profile/ProfileBanner.svelte
+++ src/components/profile/ProfileBanner.svelte
@@ -4,6 +4,7 @@ Banner
 -->
 
 <script lang="ts">
+	import { Image } from "@unpic/svelte";
 	import type { HTMLAttributes } from "svelte/elements";
 
 	interface $$Props extends HTMLAttributes<HTMLDivElement> {
@@ -15,6 +16,14 @@ Banner
 
 <div {...$$restProps}>
 	<div class="front-themed rounded border p-2 font-extrabold">
-		<div>{name}</div>
+		<div class="text-5xl">{name}</div>
+		<Image
+			alt="A lovely picture of me"
+			class="rounded"
+			height="{350}"
+			layout="fixed"
+			src="https://jarnowieman.nl/assets/profile_picture.png"
+			width="{300}"
+		/>
 	</div>
 </div>
