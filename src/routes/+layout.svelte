a834afc9f3ea8d0c5a9eb20d5a3ed6266930a33e
jarno <wiemanboy@gmail.com>
2024-07-20 15:38:44 +0200

    ADD: Google Analytics gtag within a svelte component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
 	import "./app.scss";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			// eslint-disable-next-line no-undef
+			dataLayer.push(arguments);
+		}
+
+		gtag("js", new Date());
+
+		gtag("config", "G-Z1ZPBZ2BFE");
+	});
 </script>
 
 <div class="bg-themed min-h-screen">
