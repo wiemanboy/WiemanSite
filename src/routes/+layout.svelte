ce4c6c6edfd27e45beef6e5c6e057937222a86bb
jarno <wiemanboy@gmail.com>
2024-07-20 15:46:25 +0200

    ADD: Google Analytics gtag within head (idk where to place it)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,5 @@
 <script>
 	import "./app.scss";
-	import { onMount } from "svelte";
-
-	onMount(() => {
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			// eslint-disable-next-line no-undef
-			dataLayer.push(arguments);
-		}
-
-		gtag("js", new Date());
-
-		gtag("config", "G-Z1ZPBZ2BFE");
-	});
 </script>
 
 <div class="bg-themed min-h-screen">
