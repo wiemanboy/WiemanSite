550a778ecf4d1b484dc6fcb6b07fedf5528b7bef
jarno <wiemanboy@gmail.com>
2024-10-03 11:35:45 +0200

    UPDATE: dependencies to allow coverage to be generated

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import { configDefaults, coverageConfigDefaults, defineConfig } from "vitest/con
 
 export default defineConfig({
 	plugins: [sveltekit(), Icons({ compiler: "svelte", autoInstall: true })],
+	build: {
+		sourcemap: true,
+	},
 	test: {
 		include: ["tests/**/*.{test,spec}.{js,ts}"],
 		exclude: [...configDefaults.exclude, "tests/**/*.integration.{test,spec}.{js,ts}"],
 		coverage: {
 			reportsDirectory: "test-results/coverage",
 			exclude: [...coverageConfigDefaults.exclude, "**/*.config.{js,ts}", "**/i18n.ts"],
+			provider: "istanbul",
 		},
 	},
 	server: {
