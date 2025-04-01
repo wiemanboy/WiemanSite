6334027bf5cdfa7739e4bb64e6fac38f6c68c8da
jarno <wiemanboy@gmail.com>
2025-04-01 19:12:14 +0200

    UPDATE: remove api as it was too complicated

--- inversify.config.ts
+++ inversify.config.ts
@@ -1,14 +1,9 @@
 import "reflect-metadata";
 import { Container } from "inversify";
-import { env } from "$env/dynamic/public";
-import ApiProfileRepository from "$lib/data/profiles/ApiProfileRepository";
 import FetchApiClient from "$lib/network/FetchApiClient";
 import types from "./src/lib/types";
 
 const container = new Container();
-container.bind(types.apiClient).toConstantValue(new FetchApiClient(env.PUBLIC_API_BASE_URL));
-container
-	.bind(types.profileRepository)
-	.toConstantValue(new ApiProfileRepository(container.get(types.apiClient)));
+container.bind(types.apiClient).toConstantValue(new FetchApiClient("https://dummyjson.com"));
 
 export { container };
--- src/routes/[[locale=supportedLocale]]/+page.svelte
+++ src/routes/[[locale=supportedLocale]]/+page.svelte
@@ -2,53 +2,35 @@
 
 <script lang="ts">
 	import { t } from "svelte-i18n";
-	import { page } from "$app/stores";
-	import { Bio, Image, Loading, ProfileHeader, SkillList, SocialList } from "$lib/components";
-	import container from "$lib/container";
-	import type ProfileRepository from "$lib/data/profiles/ProfileRepository";
-	import type ProfileDto from "$lib/dtos/profile/ProfileDto";
-	import types from "$lib/types";
-
-	let locale = $state($page.params.locale);
-	const profileRepository = container.get<ProfileRepository>(types.profileRepository);
-	const profile: Promise<ProfileDto> = profileRepository.getProfileByName("wiemanboy", locale || "en");
+	import { Bio, Image, ProfileHeader, SkillList, SocialList } from "$lib/components";
 </script>
 
 <main class="flex justify-center">
-	{#await profile}
-		<div class="h-screen w-screen flex flex-col justify-center">
-			<div class="flex justify-center text-6xl">
-				<Loading />
-			</div>
+	<div class="mx-4 flex flex-col gap-2 max-2 max-w-[1500px]">
+		<div class="min-h-screen my-2 flex justify-center">
+			<ProfileHeader class="flex flex-col justify-center"
+						   name={$t("profile.bio.name")} />
 		</div>
-	{:then profile}
-		<div class="mx-4 flex flex-col gap-2 max-2 max-w-[1500px]">
-			<div class="min-h-screen my-2 flex justify-center">
-				<ProfileHeader class="flex flex-col justify-center"
-							   name={`${profile.firstName} ${profile.lastName}`} />
-			</div>
 
-			<Bio class="min-h-screen flex flex-col justify-center mt-32" content={profile.description.content}
-				 title={$t('profile.bio.who.i')}>
-				{#snippet image()}
-					<Image
-						alt={$t('profile.bio.profilePictureAlt')}
-						height={350}
-						key="profile_picture"
-						width={300}
-					/>
-				{/snippet}
-			</Bio>
+		<Bio class="min-h-screen flex flex-col justify-center mt-32" content={$t("profile.bio.description")}
+			 title={$t('profile.bio.who.i')}>
+			{#snippet image()}
+				<Image
+					alt={$t('profile.bio.profilePictureAlt')}
+					height={350}
+					key="profile_picture"
+					width={300}
+				/>
+			{/snippet}
+		</Bio>
 
-			<SkillList class="mt-56 min-h-screen flex flex-col justify-center" skills={profile.skillSections} />
+		<SkillList class="mt-56 min-h-screen flex flex-col justify-center" skills={$t("profile.skills.sections")} />
 
-			<div class="flex justify-center mt-56">
-				<SocialList class="min-h-screen flex flex-col justify-center text-xl" socials={profile.socials} />
-			</div>
+		<div class="flex justify-center mt-56">
+			<SocialList class="min-h-screen flex flex-col justify-center text-xl"
+						socials={$t("profile.socials.items")} />
 		</div>
-	{:catch error}
-		<div>{error.message}</div>
-	{/await}
+	</div>
 </main>
 
 <svelte:head>
